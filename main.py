3f674f0dad8bdf3f6bf9efc5c7686a6445478d92
Proskurova <proskurovalyuda@gmail.com>
2022-08-28 17:41:05 +0300

    added the start commands

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ def check_sub_channel(chat_member):
     return chat_member['status'] != "left"
 
 
+@dp.message_handler(commands=['start'], commands_prefix="/")
+async def start(message: types.Message):
+    await message.reply("Привет!\nИнструкция по использованию бота:\n\
+1️⃣ Добавьте бота в канал со всеми правами администратора;\n2️⃣ Добавьте бота в чаты со всеми правами администратора;\n\
+Бот подключён. Теперь новые участники чата смогут в нем писать сообщения только после подписки на необходимый канал.\n\
+Бот будет проверять подписан ли человек на ваш канал! Если нет - бот не даст ему написать в чат."
+                        )
+
+
 @dp.message_handler(content_types=["new_chat_members"])
 async def user_joined(message: types.Message):
     await message.answer(f"{message.from_user.first_name},приветствую тебя!\nЧтобы иметь возможность писать в чат,\
@@ -31,7 +40,6 @@ async def user_joined(message: types.Message):
 async def mess_handler(message: types.Message):
     if not db.mute(message.from_user.id):
         return
-
     else:
         if check_sub_channel(await bot.get_chat_member(chat_id=cfg.CHANNEL_ID, user_id=message.from_user.id)):
             db.mute_del(message.from_user.id, 0)
