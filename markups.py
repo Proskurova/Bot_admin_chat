17cec0cae0ebdf25edf7f5f4c02a2708f63888ae
Proskurova <proskurovalyuda@gmail.com>
2022-11-04 23:25:52 +0300

    I changed title keyboard.

--- markups.py
+++ markups.py
@@ -1,6 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config as cfg
 
-btnUrlChannel = InlineKeyboardButton(text="Основной канал", url=cfg.CHANNEL_URL)
+
+btnUrlChannel = InlineKeyboardButton(text="Перейти в бот", url=cfg.BOT_URL)
 channelMenu = InlineKeyboardMarkup(row_width=1)
 channelMenu.insert(btnUrlChannel)
